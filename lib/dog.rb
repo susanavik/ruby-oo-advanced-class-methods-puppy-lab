3f107a61c362c2ed1734fc25359569bf5bf4980a
Susana <susana.m.vik@gmail.com>
2021-02-25 13:31:33 -0500

    commit tree 454a49c651a656e5599581a54c6a4057dc39d553

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+    attr_accessor :name, :save
+    @@all = [ ]
+    
+    def initialize(name)
+         @name = name
+         @save = save
+         @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear 
+    end
+
+    def self.print_all
+        @@all.each do |dog, name|
+            puts dog.name.to_s
+        end
+    end
+
+    def save
+       self.dog.all << self
+    end
+
+end
